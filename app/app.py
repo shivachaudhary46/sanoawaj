3ebd747130b7a9c7a04c08656c917088a9e3bb72
shivachaudhary46 <shivachaudhary4116@gmail.com>
2025-07-20 12:11:53 +0545

    added comments and well documentation

--- app/app.py
+++ app/app.py
@@ -1,11 +1,16 @@
 # Importing all libraries 
 from flask import Flask, jsonify, request, render_template, redirect, url_for
 
+# intialising app, Flask name 
 app = Flask(__name__)
 
+# 
 @app.route('/', methods=['POST', 'GET'])
 def home():
     return render_template("home.html")
 
-if __name__== "__main__":
+'''
+running app if an only if file name is equal to app.py 
+'''
+if __name__=="__main__":
     app.run(debug=True, host="0.0.0.0",  port=5050)
